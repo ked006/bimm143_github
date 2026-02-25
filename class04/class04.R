ddbd2ebfe556c31dd3964d2d415f8a90f5da1061
Kenny Dang <ked006@ucsd.edu>
2026-02-25 12:31:17 -0800

    classwork from BIMM143

---
+++ class04/class04.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Class 4 Intro to R"
+#' author: "Kenny Dang"
+#' date: "January 16, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x,sin(x), typ="o", col="green", lwd=3, xlab = "Is it lunch time yet?")
+
+
+
+
